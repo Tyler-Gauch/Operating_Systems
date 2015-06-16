dd1ded6e84b100618631fa986d2f77785a2b794b
= <=>
2015-06-16 11:49:15 -0400

    Assignment 3

---
+++ Assignment3/README.md
@@ -0,0 +1,4 @@
+Assignment 3
+=============
+
+Implements ps -ef | grep argv[1] | wc
---
+++ Assignment3/assignment3.c
@@ -0,0 +1,80 @@
+/////////////////////////////////////////////
+//                                         //
+//               Tyler Gauch               //
+//               Assignment 2              //
+//               June 16, 2015             //
+//                                         //
+/////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main ( int argc, char *argv[]) {
+        
+	int pfd[2];
+	int pfd2[2];
+	int pid[2];
+	
+	if(pipe(pfd) < 0) //create first pipe
+	{
+		perror("ERROR: Pipe1 Failed");
+		exit(-1);
+	}
+
+	if((pid[0] = fork()) < 0) //start next process to speak to first pipe
+	{
+		perror("ERROR: Fork1 Failed");
+		exit(-2);
+	}
+	
+	if(pid[0] == 0) //it is the child
+	{
+		close(pfd[1]);	//close the unused file descriptor
+		dup2(pfd[0], 0); //replace stdin with the pipe file descriptor
+		close(pfd[0]);
+		//create the process for the second pipe
+		if(pipe(pfd2) < 0)
+		{
+			perror("ERROR: Pipe2 Failed");
+			exit(-5);
+		}
+		if((pid[1] = fork()) < 0)
+		{
+			perror("ERROR: Fork2 Failed");
+			exit(-6);
+		}
+
+		if(pid[1] == 0) //if it is a child process
+		{
+			close(pfd2[1]); //close unused file descriptor
+			dup2(pfd2[0], 0); //replace stdin with the pipe file descriptor
+			close(pfd2[0]);
+			execlp("wc", "wc", (char *) 0);
+			exit(-7);
+		}
+		else
+		{
+			close(pfd2[0]); //close unused file descriptor
+			dup2(pfd2[1], 1);
+			close(pfd2[1]);
+			execlp("grep", "grep", argv[1], (char *) 0); //this starts a new process and only
+							//returns if there was an error
+			perror("ERROR: GREP Falied");
+			exit(-3);
+		}
+	}
+	else //it is the parent
+	{
+		close(pfd[0]); //close the unused file descriptor
+		dup2(pfd[1], 1);//replace stdout with the pipe file descriptor
+		close(pfd[1]); // close the old descriptor
+		execlp("ps", "ps", "-ef", (char *) 0); //this starts a new process and only
+							//returns is there was an error
+		perror("ERROR: LS Failed");
+		exit(-4);
+	}
+
+	return 0;
+}
